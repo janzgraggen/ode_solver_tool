b1bd502e49c316dbedf1e2325d89c42205cace98
Jan <jan.zgraggen@epfl.ch>
2024-12-03 23:59:56 +0100

    Polimorphism for the run function -> call common method on casted class after it is determied which type(child) the Solver is, Added/fixed pipeline for Implicit solver in the run function

--- src/Runner/Runner.cc
+++ src/Runner/Runner.cc
@@ -1,6 +1,8 @@
 #include "Runner.hh"
 #include <iostream>
 #include <memory>
+#include <typeinfo>
+
 #include "../Ode/ForwardEuler.hh"
 #include "../Ode/RungeKutta.hh"
 #include "../Ode/AdamsBashforth.hh"
@@ -9,6 +11,8 @@
 #include "../LinSysStruct.hh"
 
 #include "../config/Config.hh"
+
+using str = std::string;
 
 //constructor
 Runner::Runner(const std::string& config_file) : 
@@ -21,49 +25,31 @@ Runner::~Runner() {}
 //run method
 
 Eigen::VectorXd Runner::run() {
+    OdeSolver* Solver = nullptr;
     if (Rdr.getSolverType() == "Explicit") {
         if (Rdr.getExplicitSettings().method == "ForwardEuler") {
-            FwdEuler Solver;
-            Solver.SetStepSize(Rdr.getOdeSettings().step_size);
-            Solver.SetTimeInterval(Rdr.getOdeSettings().initial_time, Rdr.getOdeSettings().final_time);
-            Solver.SetInitialValue(Rdr.getOdeSettings().initial_value);
-            Solver.SetRightHandSide(f);
-            return Solver.SolveODE(std::cout);
+            Solver = new FwdEuler();
 
         } else if (Rdr.getExplicitSettings().method == "RungeKutta") {
             // If order is provided
             if (Rdr.getExplicitSettings().RungeKutta_order.has_value()) {
-                RungeKutta Solver(Rdr.getExplicitSettings().RungeKutta_order.value());
-                Solver.SetStepSize(Rdr.getOdeSettings().step_size);
-                Solver.SetTimeInterval(Rdr.getOdeSettings().initial_time, Rdr.getOdeSettings().final_time);
-                Solver.SetInitialValue(Rdr.getOdeSettings().initial_value);
-                Solver.SetRightHandSide(f);
-                return Solver.SolveODE(std::cout);
-
+                Solver = new RungeKutta(Rdr.getExplicitSettings().RungeKutta_order.value());
                 // If coefficients a, b, c are provided
             } else if (Rdr.getExplicitSettings().RungeKutta_coefficients_a.has_value() &&
                        Rdr.getExplicitSettings().RungeKutta_coefficients_b.has_value() &&
                        Rdr.getExplicitSettings().RungeKutta_coefficients_c.has_value()) {
-                RungeKutta Solver(Rdr.getExplicitSettings().RungeKutta_coefficients_a.value(),
-                                  Rdr.getExplicitSettings().RungeKutta_coefficients_b.value(),
-                                  Rdr.getExplicitSettings().RungeKutta_coefficients_c.value());
-                Solver.SetStepSize(Rdr.getOdeSettings().step_size);
-                Solver.SetTimeInterval(Rdr.getOdeSettings().initial_time, Rdr.getOdeSettings().final_time);
-                Solver.SetInitialValue(Rdr.getOdeSettings().initial_value);
-                Solver.SetRightHandSide(f);
-                return Solver.SolveODE(std::cout);
-                       } else {
-                           std::cout << "Invalid RungeKutta settings" << std::endl;
-                           return Eigen::VectorXd();  // Return empty vector for failure
-                       }
+                Solver = new RungeKutta(
+                    Rdr.getExplicitSettings().RungeKutta_coefficients_a.value(),
+                    Rdr.getExplicitSettings().RungeKutta_coefficients_b.value(),
+                    Rdr.getExplicitSettings().RungeKutta_coefficients_c.value());
+            } else {
+                std::cout << "Invalid RungeKutta settings" << std::endl;
+                return Eigen::VectorXd();  // Return empty vector for failure
+            }
         } else if (Rdr.getExplicitSettings().method == "AdamsBashforth") {
             if (Rdr.getExplicitSettings().AdamsBashforth_max_order.has_value()){
-                AdamsBashforth Solver(Rdr.getExplicitSettings().AdamsBashforth_max_order.value());
-                Solver.SetStepSize(Rdr.getOdeSettings().step_size);
-                Solver.SetTimeInterval(Rdr.getOdeSettings().initial_time, Rdr.getOdeSettings().final_time);
-                Solver.SetInitialValue(Rdr.getOdeSettings().initial_value);
-                Solver.SetRightHandSide(f);
-                return Solver.SolveODE(std::cout);
+                Solver = new AdamsBashforth(
+                    Rdr.getExplicitSettings().AdamsBashforth_max_order.value());
             } else {
                 std::cout << "Invalid AdamsBashforth settings" << std::endl;
                 return Eigen::VectorXd();  // Return empty vector for failure
@@ -75,17 +61,13 @@ Eigen::VectorXd Runner::run() {
 
     } else if (Rdr.getSolverType() == "Implicit") {
         if (Rdr.getImplicitSettings().method == "BackwardEuler") {
-            BackwardEuler Solver;
-            Solver.SetStepSize(Rdr.getOdeSettings().step_size);
-            Solver.SetTimeInterval(Rdr.getOdeSettings().initial_time, Rdr.getOdeSettings().final_time);
-            Solver.SetInitialValue(Rdr.getOdeSettings().initial_value);
-            Solver.SetRightHandSide(f);
-            Solver.SetRhsIsLinear(Rdr.getImplicitSettings().rhs_is_linear);
-            if (Solver.GetRhsIsLinear()) {
-                Solver.SetRhsSystem(Rdr.getImplicitSettings().rhs_system.value());
+            Solver = new BackwardEuler();
+            dynamic_cast<BackwardEuler*>(Solver)->SetRhsIsLinear(Rdr.getImplicitSettings().rhs_is_linear);
+            dynamic_cast<BackwardEuler*>(Solver)->SetLinearSystemSolver(Rdr.getImplicitSettings().linear_system_solver.value());
+            if (dynamic_cast<BackwardEuler*>(Solver)->GetRhsIsLinear()) {
+                dynamic_cast<BackwardEuler*>(Solver)->SetRhsSystem(Rdr.getImplicitSettings().rhs_system.value());
             } else {
-                // Replace with nonlinear settings
-
+                dynamic_cast<BackwardEuler*>(Solver)->SetRootFinder(Rdr.getImplicitSettings().root_finder.value());
             }
         } else {
             std::cout << "Invalid implicit solver method" << std::endl;
@@ -96,4 +78,25 @@ Eigen::VectorXd Runner::run() {
         std::cout << "Invalid solver type" << std::endl;
         return Eigen::VectorXd();  // Return empty vector for failure
     }
+    
+    if (Solver == nullptr) {
+        std::cout << "Solver not created" << std::endl;
+        delete Solver;
+        return Eigen::VectorXd();  // Return empty vector for failure
+    } else {
+        
+    
+        Solver->SetStepSize(Rdr.getOdeSettings().step_size);
+        Solver->SetTimeInterval(Rdr.getOdeSettings().initial_time, Rdr.getOdeSettings().final_time);
+        Solver->SetInitialValue(Rdr.getOdeSettings().initial_value);
+        Solver->SetRightHandSide(f); 
+        Eigen::VectorXd Result  = Solver->SolveODE(std::cout);
+        
+        // Clean up
+        delete Solver;  // Safely delete the dynamically allocated object
+        Solver = nullptr;  // Best practice to avoid dangling pointers
+
+        return Result;
+    
+}
 }
